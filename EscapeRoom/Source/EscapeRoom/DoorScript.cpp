5700e46832f54b97e784469c3c7a4903450d8819
Justin Tyrrell <justinedwardtyrrell@gmail.com>
2018-05-28 23:43:22 -0700

    Adding DoorScript and Enhancing PositionReport.

--- EscapeRoom/Source/EscapeRoom/DoorScript.cpp
+++ EscapeRoom/Source/EscapeRoom/DoorScript.cpp
@@ -1,36 +1,33 @@
 // // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
-#include "PositionReport.h"
+#include "DoorScript.h"
 #include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UDoorScript::UDoorScript()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
+	// ...
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UDoorScript::BeginPlay()
 {
 	Super::BeginPlay();
 
 	AActor* thisOwner = GetOwner();
-	FString objName = thisOwner->GetName();
-	FString objPos = thisOwner->GetTransform().GetLocation().ToString();
-//	FString objPos = "Coming Soon!";// thisOwner->GetActorTransform().TransformPosition.ToString();
-
-	UE_LOG(LogTemp, Warning, TEXT("%s Reporting Position: %s"), *objName, *objPos);
-	
+	FRotator newVec = FRotator(0.0f, 70.0f, 0.0f);
+	thisOwner->SetActorRotation(newVec);
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UDoorScript::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
